c8470fd008acb0bca5e0d7d4edd81238d2d3b51d
merly267 <harding.em@gmail.com>
2024-02-05 18:11:04 +0000

    Move die styles to component

--- src/components/BaseDie.vue
+++ src/components/BaseDie.vue
@@ -17,3 +17,35 @@ export default {
   >
   <span v-else class="die">?</span>
 </template>
+
+<style>
+.die {
+  display: inline-block;
+  height: var(--die-size);
+  width: var(--die-size);
+  line-height: var(--die-size);
+  font-weight: bold;
+  color: var(--die-text);
+  background-color: var(--die-bg);
+}
+
+.dice li .die {
+  margin-left: 0.5em;
+}
+
+.dice li:first-of-type .die {
+  margin-left: 0;
+}
+
+.die.rolled {
+  background-color: var(--rolled-bg);
+}
+
+.challenge .success {
+  background-color: var(--success-bg);
+}
+
+.challenge .failure {
+  background-color: var(--failure-bg);
+}
+</style>
