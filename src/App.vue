d68e2b2d1b642fdca4afc7a29c74cf045525baac
merly267 <harding.em@gmail.com>
2024-02-06 18:16:25 +0000

    switch between character and move on app

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,17 @@ export default {
   components: {
     CharacterSheet,
     GenericMove
-  }
+  },
+  data: () => ({
+    currentTab: 'CharacterSheet',
+    tabs: ['CharacterSheet', 'GenericMove']
+  })
 }
 </script>
 
 <template>
   <h1>Solo RPG</h1>
 
-  <CharacterSheet />
-
-  <GenericMove />
-  <MoveLayout>
-    <template #stats>
-      <StatsList :selected="selectedStat" @setSelected="setSelectedStat" />
-    </template>
-    <template #dice>
-      <button type="button" @click="rollAllDice">
-        <h2>Action score</h2>
-        <p>
-          <BaseDie :die="actionDie" />
-          +
-          <span v-if="selectedStat.score">{{ selectedStat.score }}</span>
-          <span v-else>?</span>
-          + <span class="add">?</span> =
-          <span v-if="actionScore">{{ actionScore }}</span>
-          <span v-else>?</span>
-        </p>
-        <div class="challenge">
-          <h2>Challenge dice</h2>
-          <ul class="dice">
-            <li v-for="die in challengeDice" :key="die.id">
-              <BaseDie :die="die" />
-            </li>
-          </ul>
-        </div>
-      </button>
-    </template>
-  </MoveLayout>
+  <button v-for="tab in tabs" :key="tab" @click="currentTab = tab">{{ tab }}</button>
+  <component :is="currentTab" />
 </template>
