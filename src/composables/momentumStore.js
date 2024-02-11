7517db62a503045b447dea4baf104ba7d913a7f8
merly267 <harding.em@gmail.com>
2024-02-11 11:34:22 +0000

    Add reset value

--- src/composables/momentumStore.js
+++ src/composables/momentumStore.js
@@ -1,5 +1,7 @@
 import { ref } from 'vue'
-export const momentum = ref(0)
+
+export const momentumResetValue = ref(2)
+export const momentum = ref(momentumResetValue)
 
 export const addMomentum = (x) => {
   momentum.value += x
