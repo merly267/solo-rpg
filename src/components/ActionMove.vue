1610c42566b6f560780cbc44fbd5592f9e8b0f6a
merly267 <harding.em@gmail.com>
2024-03-31 20:02:28 +0100

    Pass selected stat to move component

---
+++ src/components/ActionMove.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ActionDie from '@/components/ActionDie.vue'
+import ChallengeDice from '@/components/ChallengeDice.vue'
+const props = defineProps({
+  title: {
+    type: String
+  },
+  stat: {
+    type: Number
+  }
+})
+</script>
+<template>
+  <h2>{{ props.title }}</h2>
+  <slot></slot>
+  <h3>Action Score</h3>
+  <ActionDie />
+  + <span v-if="stat">{{ stat }}</span>
+  <span v-else>?</span>
+  + <span v-if="moveAdds > 0">{{ moveAdds }}</span>
+  <span v-else>?</span>
+  =
+  <!-- <span v-if="actionScore"
+    ><strong>{{ actionScore }}</strong></span
+  >
+  <span v-else>?</span> -->
+  <ChallengeDice />
+</template>
--- src/views/moves/GatherInformation.vue
+++ src/views/moves/GatherInformation.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { stats as statsList } from '@/composables/useCharacterStats.js'
+import ActionMove from '@/components/ActionMove.vue'
+
+const title = 'Gather Information'
+
+const selectedStat = statsList.value.find((stat) => stat.name === 'Wits')
+</script>
+
 <template>
-  <p>New Gather info move</p>
+  <ActionMove :title="title" :stat="selectedStat.score">
+    <p>This is the move instructions passed as slots</p>
+  </ActionMove>
 </template>
