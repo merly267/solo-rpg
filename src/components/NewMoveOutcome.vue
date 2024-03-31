dd86a323caead40c68dd47c3aa12d72f2e20d0fd
merly267 <harding.em@gmail.com>
2024-03-31 21:14:51 +0100

    Port matches to new moveoutcome component

--- src/components/NewMoveOutcome.vue
+++ src/components/NewMoveOutcome.vue
@@ -21,9 +21,26 @@ const outcome = computed(() => {
     return null
   }
 })
+const match = computed(() => {
+  if (challengeDice.value[0].result) {
+    const toMatch = challengeDice.value[0].result
+    const matches = challengeDice.value.filter((die) => die.result === toMatch)
+    if (matches.length > 1) {
+      if (toMatch === 10) {
+        return ' and a match on 10, the worst possible result...'
+      } else {
+        return ' and a match!'
+      }
+    } else {
+      return null
+    }
+  } else {
+    return null
+  }
+})
 </script>
 <template>
   <div v-if="actionDie.result">
-    <h3>{{ outcome }}</h3>
+    <h3>{{ outcome }}{{ match }}</h3>
   </div>
 </template>
