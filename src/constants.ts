f23c0141017b649b81856f554e8012a8c29b9050
merly267 <harding.em@gmail.com>
2024-07-28 10:57:08 +0100

    Update types

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const statNames = ['Edge', 'Heart', 'Iron', 'Shadow', 'Wits'] as const
+
+// export const outcomes = {
+//   strong: {
+//     label: 'Strong hit'
+//   },
+//   weak: {
+//     label: 'Weak hit'
+//   },
+//   miss: {
+//     label: 'Miss'
+//   }
+// }
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,21 @@
+import type { statNames } from './constants'
+
 export type Stat = {
-  name: string
+  name: StatName
   score: number
+  // set: boolean
 }
 
 export type Die = {
   id: string
   max: number
+  rolled: boolean
   result: number | null
   cancelled: boolean
   isSuccess?: boolean | null
   isCancellable?: boolean | null
 }
+
+type StatName = (typeof statNames)[number]
 
 // export type OutcomeStatus = 'Strong hit' | 'Weak hit' | 'Miss'
