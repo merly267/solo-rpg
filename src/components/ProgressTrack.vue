d6bae84296311bd96e1c76a116e8a7d78eaea852
merly267 <harding.em@gmail.com>
2024-08-04 22:10:00 +0100

    Style filled progress boxes

--- src/components/ProgressTrack.vue
+++ src/components/ProgressTrack.vue
@@ -5,12 +5,24 @@ type PropTypes = {
 
 const props = defineProps<PropTypes>()
 const maxTrack = 10
+
+const boxStyle = (index: number, progress: number) => {
+  if (index <= progress) {
+    return 'filled'
+  }
+  return
+}
 </script>
 
 <template>
   Progress is {{ props.progress }}
   <div class="progress-track">
-    <div v-for="index in maxTrack" :key="index" class="box"></div>
+    <div
+      v-for="index in maxTrack"
+      :key="index"
+      class="box"
+      :class="boxStyle(index, props.progress)"
+    ></div>
   </div>
 </template>
 
@@ -18,12 +30,22 @@ const maxTrack = 10
 .progress-track {
   display: grid;
   max-width: 20rem;
-  border: 1px solid var(--die-bg);
 
   .box {
     grid-row: 1/1;
     aspect-ratio: 1/1;
-    border: 1px solid var(--die-bg);
+    border: 2px solid var(--die-bg);
+    border-left-width: 0;
+    &:first-child {
+      border-left-width: 2px;
+    }
+    &.filled {
+      border-color: var(--app-text);
+      border-color: white;
+      background-color: var(--grey-text);
+      /* background-color: white;
+      background-color: var(--app-text); */
+    }
   }
 }
 </style>
