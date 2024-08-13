cae314254cfa87418c177a5ebcbc3b1ab2de5bc3
merly267 <harding.em@gmail.com>
2024-08-13 19:13:19 +0100

    Refactor character sheet to use new debilities store

--- src/views/CharacterSheet.vue
+++ src/views/CharacterSheet.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import {
-  backgroundVow,
-  character,
-  experience,
-  debilities,
-  debilitiesTotal
-} from '@/composables/useCharacterStats'
-
+import { backgroundVow, character, experience } from '@/composables/useCharacterStats'
 import StatsList from '@/components/StatsList.vue'
 import { useMomentumStore } from '@/stores/MomentumStore'
+import { useDebilitiesStore } from '@/stores/DebilitiesStore'
+import { debilityCategoriesList } from '@/constants'
 
 const momentumStore = useMomentumStore()
+const debilitiesStore = useDebilitiesStore()
 </script>
 
 <template>
@@ -24,15 +20,17 @@ const momentumStore = useMomentumStore()
   <p>Health: {{ character.health }}</p>
   <p>Spirit: {{ character.spirit }}</p>
   <p>Supply: {{ character.supply }}</p>
-  <h2>Debilities: {{ debilitiesTotal }}</h2>
-  <ul v-if="debilitiesTotal">
-    <li v-for="group in debilities" :key="group.group">
-      <h3>{{ group.group }}</h3>
+  <h2>Debilities: {{ debilitiesStore.debilitiesTotal }}</h2>
+  <ul v-if="debilitiesStore.debilitiesTotal">
+    <li v-for="category in debilityCategoriesList" :key="category">
+      <h3>{{ category }}</h3>
       <ul>
         <li
+          v-for="(debility, index) in debilitiesStore.newDebilities.filter(
+            (deb) => deb.category === category
+          )"
+          :key="`deb-${index}`"
           class="debility"
-          v-for="debility in group.debilitiesList"
-          :key="debility.name"
           :class="{ marked: debility.status }"
         >
           {{ debility.name }}: {{ debility.status }}
