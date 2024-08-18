4ddd54fb9e17012273c7398181d0167891f64fda
merly267 <harding.em@gmail.com>
2024-08-18 16:40:30 +0100

    Add vow selector to fulfill vow

--- src/views/moves/FulfillVow.vue
+++ src/views/moves/FulfillVow.vue
@@ -5,10 +5,40 @@ import ProgressMove from '@/components/ProgressMove.vue'
 import AdjustMomentumButton from '@/components/AdjustMomentumButton.vue'
 import MoveOutcome from '@/components/MoveOutcome.vue'
 import MoveLayout from '@/components/MoveLayout.vue'
+import TrackInfo from '@/components/TrackInfo.vue'
 import type { StatName } from '@/types'
 import { movesList } from '@/moves'
+import { useProgressTrackStore } from '@/stores/ProgressTrackStore'
 
 const move = movesList.fulfillVow
+const swearMove = movesList.swearVow
+
+const progressTrackStore = useProgressTrackStore()
+
+const noVow = computed(() => {
+  if (progressTrackStore.vows.length) {
+    return false
+  }
+  return true
+})
+
+const multipleVows = computed(() => {
+  if (progressTrackStore.vows.length <= 1) {
+    return false
+  }
+  return true
+})
+
+const selectedVowUuid = ref(progressTrackStore.lastTouchedVow)
+
+const selectedVow = computed(() => {
+  return progressTrackStore.vows.find((vow) => vow.uuid === selectedVowUuid.value)
+})
+
+const setLastTouched = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  progressTrackStore.setLastTouched(target.value)
+}
 </script>
 <template>
   <MoveLayout>
@@ -18,6 +48,31 @@ const move = movesList.fulfillVow
           When you <strong>{{ move.trigger }}</strong
           >, roll the challenge dice and compare to your progress. Momentum is ignored on this roll.
         </p>
+        <div v-if="noVow">
+          First you must
+          <button @click="$router.push(`/moves/${swearMove.slug}`)">
+            {{ swearMove.title }}
+          </button>
+        </div>
+        <div v-if="multipleVows">
+          <label for="vow-select">Choose a vow:</label>
+          <select name="vows" id="vow-select" v-model="selectedVowUuid" @change="setLastTouched">
+            <option
+              v-for="vow in progressTrackStore.vows"
+              :key="`vow-${vow.uuid}`"
+              :value="vow.uuid"
+            >
+              {{ vow.name }}
+            </option>
+          </select>
+        </div>
+        <div v-if="selectedVow">
+          <TrackInfo
+            :name="selectedVow.name"
+            :rank="selectedVow.rank"
+            :progress="selectedVow.progress"
+          />
+        </div>
       </ProgressMove>
     </template>
   </MoveLayout>
