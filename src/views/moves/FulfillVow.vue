9b0edc215b8fd0c7a3c4eee54cdb90e38a8c252a
merly267 <harding.em@gmail.com>
2024-08-18 16:14:11 +0100

    Add fulfill vow text

--- src/views/moves/FulfillVow.vue
+++ src/views/moves/FulfillVow.vue
@@ -1,1 +1,22 @@
-<template>Fulfill vow move</template>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { stats as statsList } from '@/composables/useCharacterStats.js'
+import ActionMove from '@/components/ActionMove.vue'
+import AdjustMomentumButton from '@/components/AdjustMomentumButton.vue'
+import MoveOutcome from '@/components/MoveOutcome.vue'
+import MoveLayout from '@/components/MoveLayout.vue'
+import type { StatName } from '@/types'
+import { movesList } from '@/moves'
+
+const move = movesList.fulfillVow
+</script>
+<template>
+  <MoveLayout
+    ><template #text>
+      <p>
+        When you <strong>{{ move.trigger }}</strong
+        >, roll the challenge dice and compare to your progress. Momentum is ignored on this roll.
+      </p>
+    </template></MoveLayout
+  >
+</template>
