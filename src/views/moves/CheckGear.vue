c7b35653e8b91f5e547554afca31e1d3aeae35a5
merly267 <harding.em@gmail.com>
2024-09-08 10:36:21 +0100

    Add actionmove

--- src/views/moves/CheckGear.vue
+++ src/views/moves/CheckGear.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 import ActionMove from '@/components/ActionMove.vue'
 import MoveLayout from '@/components/MoveLayout.vue'
+import { useCharacterStore } from '@/stores/CharacterStore'
+import { movesList } from '@/moves'
+
+const move = movesList.checkGear
+
+const moveAdds = 0
+
+const characterStore = useCharacterStore()
+
+// need to choose between hold and personal supply
+const selectedTrack = 4
 </script>
 
 <template>
   <MoveLayout>
-    <template #text> Move goes here </template>
+    <template #text>
+      <ActionMove :title="move.title" :stat="selectedTrack" :adds="moveAdds">
+        <p>
+          When you <strong>{{ move.trigger }}</strong
+          >, roll +supply ({{ selectedTrack }}).
+        </p>
+      </ActionMove>
+    </template>
   </MoveLayout>
 </template>
