d369469693f59e2199ecd0e529606c21e74ca33b
merly267 <harding.em@gmail.com>
2024-09-08 10:45:35 +0100

    Choose supply type

--- src/views/moves/CheckGear.vue
+++ src/views/moves/CheckGear.vue
@@ -1,26 +1,56 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import ActionMove from '@/components/ActionMove.vue'
 import MoveLayout from '@/components/MoveLayout.vue'
 import { useCharacterStore } from '@/stores/CharacterStore'
 import { movesList } from '@/moves'
 
 const move = movesList.checkGear
-
-const moveAdds = 0
-
 const characterStore = useCharacterStore()
 
-// need to choose between hold and personal supply
-const selectedTrack = 4
+const selectedSupplyType = ref<string>('Equipped')
+
+const selectedSupplyScore = computed(() => {
+  if (selectedSupplyType.value === 'Hold') {
+    return characterStore.hold
+  } else {
+    return characterStore.supply
+  }
+})
+
+const moveAdds = 0
 </script>
 
 <template>
   <MoveLayout>
     <template #text>
-      <ActionMove :title="move.title" :stat="selectedTrack" :adds="moveAdds">
+      <pre>selectedSupplyType: {{ selectedSupplyType }}</pre>
+      <ActionMove :title="move.title" :stat="selectedSupplyScore" :adds="moveAdds">
+        <fieldset>
+          <div>
+            <input
+              type="radio"
+              name="chooseSupply"
+              id="equipped"
+              value="Equipped"
+              v-model="selectedSupplyType"
+            />
+            <label for="equipped">Equipped supply ({{ characterStore.supply }})</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="chooseSupply"
+              id="hold"
+              value="Hold"
+              v-model="selectedSupplyType"
+            />
+            <label for="hold">Hold supply ({{ characterStore.hold }})</label>
+          </div>
+        </fieldset>
         <p>
           When you <strong>{{ move.trigger }}</strong
-          >, roll +supply ({{ selectedTrack }}).
+          >, roll +supply ({{ selectedSupplyScore }}).
         </p>
       </ActionMove>
     </template>
