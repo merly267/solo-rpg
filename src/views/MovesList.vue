d1e052f85e233ef28a7dedfa45828ac67ea9312e
merly267 <harding.em@gmail.com>
2024-10-03 18:32:14 +0100

    Group moves by type

--- src/views/MovesList.vue
+++ src/views/MovesList.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import { movesList } from '@/moves'
+import { moveTypeList } from '@/constants'
 </script>
 <template>
   <h2>Moves</h2>
-  <dl v-for="(move, index) in movesList" :key="`move-${index}`">
-    <dt>
-      <router-link :to="{ path: `/moves/${move.slug}` }" class="move">{{ move.title }}</router-link>
-    </dt>
-    <dd class="trigger">{{ move.trigger }}.</dd>
-  </dl>
+  <div v-for="moveType in moveTypeList" :key="moveType">
+    <h3>{{ moveType }}</h3>
+    <dl v-for="(move, index) in movesList" :key="`move-${index}`">
+      <div v-if="move.moveType === moveType">
+        <dt>
+          <router-link :to="{ path: `/moves/${move.slug}` }" class="move">{{
+            move.title
+          }}</router-link>
+        </dt>
+        <dd class="trigger">{{ move.trigger }}.</dd>
+      </div>
+    </dl>
+  </div>
 </template>
 
 <style scoped>
