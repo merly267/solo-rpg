5cabb6187ef65e6fb4104a50c7b601108ed87ae7
merly267 <harding.em@gmail.com>
2024-10-03 18:50:48 +0100

    Show/hide moves by type (not persistent)

--- src/views/MovesList.vue
+++ src/views/MovesList.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { movesList } from '@/moves'
 import { moveTypeList } from '@/constants'
+import type { MoveType } from '@/types'
+
+let visibleSections = ref<MoveType[]>([])
 </script>
 <template>
   <h2>Moves</h2>
+  <button @click="visibleSections = [...new Set([...visibleSections, ...moveTypeList])]">
+    Show all
+  </button>
+  <button @click="visibleSections = []">Hide all</button>
   <div v-for="moveType in moveTypeList" :key="moveType">
-    <h3>{{ moveType }}</h3>
-    <dl v-for="(move, index) in movesList" :key="`move-${index}`">
-      <div v-if="move.moveType === moveType">
-        <dt>
-          <router-link :to="{ path: `/moves/${move.slug}` }" class="move">{{
-            move.title
-          }}</router-link>
-        </dt>
-        <dd class="trigger">{{ move.trigger }}.</dd>
-      </div>
-    </dl>
+    <h3>
+      {{ moveType }}
+      <button
+        v-if="visibleSections.includes(moveType)"
+        @click="visibleSections = visibleSections.filter((item) => item !== moveType)"
+      >
+        Hide</button
+      ><button v-else @click="visibleSections.push(moveType)">Show</button>
+    </h3>
+    <div v-show="visibleSections.includes(moveType)">
+      <dl v-for="(move, index) in movesList" :key="`move-${index}`">
+        <div v-if="move.moveType === moveType">
+          <dt>
+            <router-link :to="{ path: `/moves/${move.slug}` }" class="move">{{
+              move.title
+            }}</router-link>
+          </dt>
+          <dd class="trigger">{{ move.trigger }}.</dd>
+        </div>
+      </dl>
+    </div>
   </div>
 </template>
 
