dcf1e7ea18387ccddb7e45a05905b5b2e52e4b2c
merly267 <harding.em@gmail.com>
2024-11-20 17:05:05 +0000

    Add Set Course component

---
+++ src/views/moves/SetCourse.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ActionMove from '@/components/ActionMove.vue'
+import AdjustMomentumButton from '@/components/AdjustMomentumButton.vue'
+import MoveOutcome from '@/components/MoveOutcome.vue'
+import MoveLayout from '@/components/MoveLayout.vue'
+</script>
+<template>
+  <MoveLayout>
+    <template #text>
+      Set a course
+      <!-- <ActionMove
+        v-if="selectedStat"
+        :title="move.title"
+        :stat="selectedStat.score"
+        :adds="moveAdds"
+        @makeMove="makeMove"
+        @clearMove="clearMove"
+      >
+        <p>
+          When you <strong>{{ move.trigger }}</strong
+          >, roll +{{ selectedStat.name }} ({{ selectedStat.score }}).
+        </p>
+      </ActionMove> -->
+    </template>
+  </MoveLayout>
+</template>
