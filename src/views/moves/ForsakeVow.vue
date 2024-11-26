84d409ad89e077fb4131909bfde0950236003695
merly267 <harding.em@gmail.com>
2024-11-26 17:48:37 +0000

    Show forsaken vow once move made

--- src/views/moves/ForsakeVow.vue
+++ src/views/moves/ForsakeVow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, toRaw } from 'vue'
 import MoveLayout from '@/components/MoveLayout.vue'
 import TrackInfo from '@/components/TrackInfo.vue'
 import { movesList } from '@/moves'
@@ -34,6 +34,19 @@ const setLastTouched = (event: Event) => {
   progressTrackStore.setLastTouched('Vow', target.value)
 }
 
+const clearedVow = ref(selectedVow.value)
+
+let moveMade = ref(false)
+
+const makeMove = () => {
+  moveMade.value = true
+}
+
+const clearVow = () => {
+  progressTrackStore.markForsaken(selectedVowUuid.value, 'Vow')
+  makeMove()
+}
+
 </script>
 <template>
   <MoveLayout>
@@ -41,15 +54,16 @@ const setLastTouched = (event: Event) => {
       <h2>{{ move.title }}</h2>
       <p>
         When you <strong>{{ move.trigger }}</strong>, 
+        <button @click="clearVow">Clear the vow</button>
       </p>
-      <div v-if="noVow">
+      <div v-if="noVow && !moveMade">
           First you must
           <button @click="$router.push(`/moves/${swearMove.slug}`)">
             {{ swearMove.title }}
           </button>
         </div>
         <div v-if="multipleVows">
-          <label for="vow-select">Choose a vow:</label>
+          <label for="vow-select">Choose a vow: </label>
           <select name="vows" id="vow-select" v-model="selectedVowUuid" @change="setLastTouched">
             <option
               v-for="vow in progressTrackStore.activeVows"
@@ -65,6 +79,14 @@ const setLastTouched = (event: Event) => {
             :name="selectedVow.name"
             :rank="selectedVow.rank"
             :progress="selectedVow.progress"
+          />
+        </div>
+        <div v-if="moveMade && clearedVow">
+          <TrackInfo
+            :name="clearedVow.name"
+            :rank="clearedVow.rank"
+            :progress="clearedVow.progress"
+            :status="clearedVow.status"
           />
         </div>
     </template>
