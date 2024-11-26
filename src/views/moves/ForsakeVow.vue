4f0c53601a84f452892bd61eda253458eefca65c
merly267 <harding.em@gmail.com>
2024-11-26 16:32:42 +0000

    Add vow selector

--- src/views/moves/ForsakeVow.vue
+++ src/views/moves/ForsakeVow.vue
@@ -1,3 +1,72 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import MoveLayout from '@/components/MoveLayout.vue'
+import TrackInfo from '@/components/TrackInfo.vue'
+import { movesList } from '@/moves'
+import { useProgressTrackStore } from '@/stores/ProgressTrackStore'
+
+const move = movesList.forsakeVow
+const swearMove = movesList.swearVow
+const progressTrackStore = useProgressTrackStore()
+
+const noVow = computed(() => {
+  if (progressTrackStore.activeVows.length) {
+    return false
+  }
+  return true
+})
+
+const multipleVows = computed(() => {
+  if (progressTrackStore.activeVows.length <= 1) {
+    return false
+  }
+  return true
+})
+
+const selectedVowUuid = ref(progressTrackStore.lastTouchedVow)
+
+const selectedVow = computed(() => {
+  return progressTrackStore.activeVows.find((vow) => vow.uuid === selectedVowUuid.value)
+})
+
+const setLastTouched = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  progressTrackStore.setLastTouched('Vow', target.value)
+}
+
+</script>
 <template>
-  Forsake vow goes here
+  <MoveLayout>
+    <template #text>
+      <h2>{{ move.title }}</h2>
+      <p>
+        When you <strong>{{ move.trigger }}</strong>, 
+      </p>
+      <div v-if="noVow">
+          First you must
+          <button @click="$router.push(`/moves/${swearMove.slug}`)">
+            {{ swearMove.title }}
+          </button>
+        </div>
+        <div v-if="multipleVows">
+          <label for="vow-select">Choose a vow:</label>
+          <select name="vows" id="vow-select" v-model="selectedVowUuid" @change="setLastTouched">
+            <option
+              v-for="vow in progressTrackStore.activeVows"
+              :key="`vow-${vow.uuid}`"
+              :value="vow.uuid"
+            >
+              {{ vow.name }}
+            </option>
+          </select>
+        </div>
+        <div v-if="selectedVow">
+          <TrackInfo
+            :name="selectedVow.name"
+            :rank="selectedVow.rank"
+            :progress="selectedVow.progress"
+          />
+        </div>
+    </template>
+  </MoveLayout>
 </template>
