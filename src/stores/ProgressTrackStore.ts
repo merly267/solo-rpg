002338a2e7fe3cfae9d3969727d07a629510edb4
merly267 <harding.em@gmail.com>
2024-11-26 12:02:11 +0000

    Fix active track filtering

--- src/stores/ProgressTrackStore.ts
+++ src/stores/ProgressTrackStore.ts
@@ -25,10 +25,10 @@ export const useProgressTrackStore = defineStore('progressTrackStore', {
   }),
   getters: {
     activeExpeditions: (state) => {
-      return state.expeditions.filter((expedition) => expedition.status === 'In progress' || 'Full')
+      return state.expeditions.filter((expedition) => expedition.status === 'In progress' || expedition.status === 'Full')
     },
     activeVows: (state) => {
-      return state.vows.filter((vow) => vow.status === 'In progress' || 'Full')
+      return state.vows.filter((vow) => vow.status === 'In progress' || vow.status === 'Full')
     }
   },
   actions: {
