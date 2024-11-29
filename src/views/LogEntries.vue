e05b9ad46f34d4874bb529ce2546cd9aff4f51e5
merly267 <harding.em@gmail.com>
2024-11-29 13:52:00 +0000

    Style log

--- src/views/LogEntries.vue
+++ src/views/LogEntries.vue
@@ -5,5 +5,7 @@ const logStore = useLogStore()
 </script>
 <template>
   <h2>Journal</h2>
-  logStore.log: <pre>{{ logStore.log }}</pre>
+  <section v-for="entry in logStore.log" :key="entry.timestamp">
+    <p><strong>{{ entry.move }}</strong><span v-if="entry.stat"> + {{ entry.stat }}</span>: {{ entry.outcome }}<span v-if="entry.match"> {{ entry.match }}</span></p>
+  </section>
 </template>
