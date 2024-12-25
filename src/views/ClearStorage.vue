c1fb1922a4542aa659a920627160fd3f4066c6a9
merly267 <harding.em@gmail.com>
2024-12-25 15:46:17 +0000

    Add clearStorage view

---
+++ src/views/ClearStorage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useCharacterStore } from '@/stores/CharacterStore'
+import { useImpactsStore } from '@/stores/ImpactsStore'
+import { useMomentumStore } from '@/stores/MomentumStore'
+import { useLegacyTrackStore } from '@/stores/LegacyTrackStore'
+
+const characterStore = useCharacterStore()
+const momentumStore = useMomentumStore()
+const impactsStore = useImpactsStore()
+const legacyTrackStore = useLegacyTrackStore()
+
+const clearAll = () => {
+  impactsStore.clearLocalStorage()
+}
+</script>
+<template>
+  <button @click="clearAll">Clear all local storage</button>
+  <hr>
+  <button @click="impactsStore.clearLocalStorage">Clear impacts</button>
+</template>
