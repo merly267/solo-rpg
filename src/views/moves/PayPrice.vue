e017f03c91a32a958212576a97cbe6c9708ae89e
merly267 <harding.em@gmail.com>
2025-12-04 21:34:42 +0000

    Add move text

--- src/views/moves/PayPrice.vue
+++ src/views/moves/PayPrice.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MoveLayout from '@/components/MoveLayout.vue'
+import { movesList } from '@/moves'
+
+const move = movesList.payPrice
+</script>
 
 <template>
-  <h2>Payyyy</h2>
+  <MoveLayout>
+    <template #text>
+      <h2>{{ move.title }}</h2>
+      <div>
+        <p>
+          When you <strong>{{ move.trigger }}</strong
+          >, choose one.
+        </p>
+        <ul>
+          <li>Make the most obvious negative outcome happen.</li>
+          <li>
+            Ask the Oracle for inspiration. Interpret the answer as a hardship or complication
+            appropriate to the situation.
+          </li>
+          <li>Roll on the table below. If the result doesn’t fit the situation, roll again.</li>
+        </ul>
+      </div>
+    </template>
+  </MoveLayout>
 </template>
